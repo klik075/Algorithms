3e7a9747f4d2db542d83dc881dc7eb47c8a4fbdf
정원우 <54103715+klik075@users.noreply.github.com>
2024-02-13 09:45:29 +0900

    [level 1] Title: 모의고사, Time: 2.07 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cs
+++ 프로그래머스/1/42840. 모의고사/모의고사.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public int[] solution(int[] answers) {
         int[] answer = new int[]{};
@@ -7,7 +8,7 @@ public class Solution {
         int[] two = new int[]{2,1,2,3,2,4,2,5};
         int[] three = new int[]{3,3,1,1,2,2,4,4,5,5};
         int[] arr = new int[]{0,0,0};
-        int max = 0;
+        
         List<int> result = new List<int>();
         for(int i = 0 ; i < answers.Length;i++)
         {
@@ -18,16 +19,9 @@ public class Solution {
             if(three[i%three.Length] == answers[i])
                 arr[2]++;
         }
-        foreach(int num in arr)
-        {
-            if(max < num)
-                max = num;
-        }
-        for(int i = 0 ; i < arr.Length; i++)
-        {
-            if(arr[i] == max)
-                result.Add(i+1);
-        }
+        if(arr.Max() == arr[0]) result.Add(1);
+        if(arr.Max() == arr[1]) result.Add(2);
+        if(arr.Max() == arr[2]) result.Add(3);
         answer = result.ToArray();
         return answer;
     }
