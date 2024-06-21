37ef556b6446bc7659c4e161636ee489db012d46
정원우 <54103715+klik075@users.noreply.github.com>
2024-06-21 21:20:23 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.19 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int answer1 = 0;
+        int answer2 = 0;
+        int num1 = 10;
+        int num2 = 10;
+        foreach(var item in num_list)
+        {
+            if(item % 2 == 0)//짝수
+            {
+                answer1 *= num1;
+                answer1 += item;
+            }
+            else//홀수
+            {
+                answer2 *= num2;
+                answer2 += item;
+            }
+            answer = answer1 + answer2;
+        }
+        return answer;
+    }
+}
