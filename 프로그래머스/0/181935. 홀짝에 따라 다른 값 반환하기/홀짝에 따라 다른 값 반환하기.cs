9f2d2a1a4f74863be373de9868e50ef33ba83ed1
정원우 <54103715+klik075@users.noreply.github.com>
2024-06-21 22:05:51 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.29 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i < n + 1 ; i++)
+        {
+            if(n % 2 == 0)
+            {
+                if(i % 2 == 0)
+                {
+                    answer += i * i;
+                }
+            }
+            else
+            {
+                if(i % 2 != 0)
+                {
+                    answer += i;
+                }
+            }
+        }
+        return answer;
+    }
+}
