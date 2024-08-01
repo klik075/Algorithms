612a99022a383a4ff348dffbd647639ba3cbda67
정원우 <54103715+klik075@users.noreply.github.com>
2024-08-01 17:47:05 +0900

    [level 0] Title: 배열 비교하기, Time: 0.30 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int result1 = 0;
+        int result2 = 0;
+        if(arr1.Length < arr2.Length)
+        {
+            answer = -1;
+        }
+        else if(arr1.Length > arr2.Length)
+        {
+            answer = 1;
+        }
+        else
+        {
+            for(int i = 0 ; i < arr1.Length;i++)
+            {
+                result1 += arr1[i];
+                result2 += arr2[i];
+            }
+            if(result1 > result2)
+            {
+                answer = 1;
+            }
+            else if(result1 < result2)
+            {
+                answer = -1;
+            }
+            else
+            {
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
