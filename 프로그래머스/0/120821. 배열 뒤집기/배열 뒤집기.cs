118c115b19fae8344c14e48b24c2b3956cd574dd
정원우 <54103715+klik075@users.noreply.github.com>
2024-08-01 17:15:34 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.72 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.Length];
+        Stack<int> stack = new Stack<int>();
+        for(int i = 0; i < num_list.Length ; i++)
+        {
+            stack.Push(num_list[i]);
+        }
+        for(int i = 0; i< answer.Length;i++)
+        {
+            answer[i] = stack.Pop();
+        }
+        return answer;
+    }
+}
