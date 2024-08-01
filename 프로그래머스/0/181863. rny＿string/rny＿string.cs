e00e24acdcbfa729a796200f3533165317e41303
정원우 <54103715+klik075@users.noreply.github.com>
2024-08-01 17:53:42 +0900

    [level 0] Title: rny_string, Time: 0.66 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public string solution(string rny_string) {
+        string answer = "";
+        answer = rny_string.Replace("m","rn");
+        return answer;
+    }
+}
