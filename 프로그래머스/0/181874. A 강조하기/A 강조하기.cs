1ce463f4277d907d42984f234f978c7c85ba405f
정원우 <54103715+klik075@users.noreply.github.com>
2024-08-02 15:33:15 +0900

    [level 0] Title: A 강조하기, Time: 0.71 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        string answer = "";
+        answer = myString.ToLower();
+        answer = answer.Replace("a","A");
+        return answer;
+    }
+}
