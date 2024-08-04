a9551de8bdfd4357c68d005b207a65fc4e666974
정원우 <54103715+klik075@users.noreply.github.com>
2024-08-04 15:08:34 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 3.58 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.Length - 5];
-        List<int> arr = new List<int>();
-        foreach(var num in num_list)
-        {
-            arr.Add(num);
-        }
+        List<int> arr = num_list.ToList();
         arr.Sort();
         for(int i = 0; i< 5;i++)
         {
