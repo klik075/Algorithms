8cee6442a2584ec58f3b498576317f7122a7d9ee
정원우 <54103715+klik075@users.noreply.github.com>
2024-08-12 03:03:04 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.25 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cs
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cs
@@ -1,53 +1,23 @@
-using System;
-using System.Collections.Generic;
 public class Solution {
     public int solution(int[,] lines) {
-        int answer = 0;
-        int[] arr = new int[2];
-        Dictionary<int,List<int>> dict = new Dictionary<int,List<int>>(); 
-        List<int> key = new List<int>();
-        for(int i =0; i < lines.GetLength(0);i++)
-        {
-            for(int j = 0 ; j < lines.GetLength(1);j++)
-            {
-                arr[j] = lines[i,j];
-            }
-            for(int z = arr[0]; z <= arr[1]; z++)
-            {
-                if(!key.Contains(z))
-                {
-                    key.Add(z);
-                }
-                if(!dict.ContainsKey(z))
-                {
-                    dict[z] = new List<int>();
-                }
-                dict[z].Add(i);
+        int[] counts = new int[201]; // -100 to 100 range, shifted by 100
+        
+        for (int i = 0; i < lines.GetLength(0); i++) {
+            int start = lines[i, 0] + 100; // Shift to ensure positive indices
+            int end = lines[i, 1] + 100;
+            
+            for (int j = start; j < end; j++) {
+                counts[j]++;
             }
         }
-        key.Sort();
-        for(int i = 0 ; i < key.Count; i++)
-        {
-            if(dict[key[i]].Count != 1)
-            {
-                if(dict.ContainsKey(key[i]+1))
-                {
-                    int count = 0;
-                    for(int k = 0; k < dict[key[i]].Count;k++)
-                    {
-                        if(dict[key[i]+1].Contains(dict[key[i]][k]))
-                        {
-                            count++;
-                        }
-                    }
-                    Console.WriteLine(count);
-                    if(count >= 2)
-                    {
-                        answer++;
-                    }
-                }
-            }                   
-        }    
-        return answer;  
+        
+        int answer = 0;
+        for (int i = 0; i < counts.Length; i++) {
+            if (counts[i] > 1) {
+                answer++;
+            }
+        }
+        
+        return answer;
     }
 }
