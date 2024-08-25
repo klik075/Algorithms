133b0a935b486a78534082bc171f261563835911
정원우 <54103715+klik075@users.noreply.github.com>
2024-08-25 13:26:09 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.18 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public bool solution(bool x1, bool x2, bool x3, bool x4) {
+        bool answer = false;
+        if(x1 == true || x2 == true)
+            answer = true;
+        else
+        {
+            answer = false;
+            return answer;
+        }
+        if(x3 == true || x4 == true)
+            answer = true;
+        else
+        {
+            answer = false;
+            return answer;
+        }
+        return answer;
+    }
+}
