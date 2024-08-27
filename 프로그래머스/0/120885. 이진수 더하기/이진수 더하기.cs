e591fcd6ca49802ad20b821f1c1bc8020922e499
정원우 <54103715+klik075@users.noreply.github.com>
2024-08-27 21:12:51 +0900

    [level 0] Title: 이진수 더하기, Time: 0.48 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public string solution(string bin1, string bin2) {
+        int num1 = Convert.ToInt32(bin1,2);
+        int num2 = Convert.ToInt32(bin2,2);
+        string answer = Convert.ToString(num1 + num2,2);
+        return answer;
+    }
+}
