3226124e8b381c120312407082438b8f4ba621f2
정원우 <54103715+klik075@users.noreply.github.com>
2024-09-09 18:42:29 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.70 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public string[] solution(string[] str_list) {
+        List<string> answer = new List<string>();
+        bool isTrigger = false;
+        for(int i = 0 ; i < str_list.Length;i++)
+        {
+            if(str_list[i] == "l" && isTrigger == false)
+            {
+                break;
+            }
+            else if(str_list[i] == "r" && isTrigger == false)
+            {
+                isTrigger = true;
+                answer.Clear();
+                continue;
+            }
+            else
+            {
+                answer.Add(str_list[i]);
+            }
+        }
+        if(answer.Count == str_list.Length)
+            return new string[]{};
+        return answer.ToArray();
+    }
+}
