ead1538d1ee03233e9e70f86c4ff4c63768a360b
정원우 <54103715+klik075@users.noreply.github.com>
2025-06-24 12:33:49 +0900

    [level 0] Title: 영어가 싫어요, Time: 1.64 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public long solution(string numbers) {
+        long answer = 0;
+        List<string> arr = new List<string>{
+        "zero", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine"
+    };
+        for(int i = 0; i < arr.Count; i++)
+        {
+            while(numbers.Contains(arr[i]))
+                numbers = numbers.Replace(arr[i],i.ToString());
+        }
+        answer = Int64.Parse(numbers);
+        return answer;
+    }
+}
