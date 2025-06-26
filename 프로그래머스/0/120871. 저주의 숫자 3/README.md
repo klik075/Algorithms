9b9f8ccaf68c7e68d1961aaaa12e22dc28622795
정원우 <54103715+klik075@users.noreply.github.com>
2025-06-26 16:24:29 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.25 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/README.md
+++ 프로그래머스/0/120871. 저주의 숫자 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 저주의 숫자 3 - 120871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120871?language=csharp) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.02 ms
+메모리: 31.5 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 26일 16:22:30
+2025년 06월 26일 16:24:27
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            answer++;
+            int mul = 1;
+            
+            while(answer >= mul)
+            {
+                if(answer % 3 == 0)
+                {
+                    answer++;
+                    continue;
+                }
+
+                int temp = answer / mul;
+                if(temp % 10 == 3)
+                {
+                    answer++;
+                    mul = 1;
+                    continue;
+                }
+
+                mul *= 10;
+            }
+        }
+        return answer;
+    }
+}
