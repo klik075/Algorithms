f2ea83efa75ec8704a6c99ec8f506986ff0b92e4
정원우 <54103715+klik075@users.noreply.github.com>
2026-09-24 04:19:21 +0900

    [level 1] Title: 3진법 뒤집기, Time: 7.77 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        List<Integer> arr = new ArrayList<>();
+        
+        while(n > 0)
+        {
+            int num = n % 3;
+            arr.add(num);
+            n = n / 3;
+        }
+        
+        int sum = IntStream.rangeClosed(1, arr.size())
+            .map(num -> {
+                int index = num - 1;
+                if(arr.get(index) == 0)
+                    return 0;
+                
+                int Multiplication = 1;
+                for(int i = arr.size() - num; i > 0; i--){
+                    Multiplication *= 3;
+                }
+                System.out.println(arr.get(index) * Multiplication);
+                return arr.get(index) * Multiplication;
+            })
+            .sum();
+        return sum;
+    }
+}
