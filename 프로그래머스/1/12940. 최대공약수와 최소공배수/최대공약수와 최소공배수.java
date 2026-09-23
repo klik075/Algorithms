b61f5792c41f7a7e96a60331692582c90d55a68f
정원우 <54103715+klik075@users.noreply.github.com>
2026-09-24 03:00:27 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 5.48 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -2,8 +2,9 @@ class Solution {
     public int[] solution(int n, int m) {
         int min = 1;
         int max = min;
+        int maxTry = n * m;
         
-        while(min < n * m)
+        while(min < maxTry)
         {
             if(min % n == 0 && min % m == 0)
                 break;
